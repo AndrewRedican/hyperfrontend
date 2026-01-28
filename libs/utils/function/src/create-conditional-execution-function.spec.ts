2a56fcd7a8221a756f72893fbf021245784d3ddd
Andrew Redican <andrew.redican.mejia@gmail.com>
2026-01-28 15:25:26 +0000

    feat(lib-function-utils): complete implementation

---
+++ libs/utils/function/src/create-conditional-execution-function.spec.ts
@@ -0,0 +1,25 @@
+import { createConditionalExecutionFunction } from './create-conditional-execution-function'
+
+describe('createConditionalExecutionFunction', () => {
+  it('executes the function when the condition is true', () => {
+    const testFunc = jest.fn((x: number) => x * 3)
+    const condition = jest.fn(() => true)
+
+    const conditionalFunc = createConditionalExecutionFunction(testFunc, condition)
+
+    expect(conditionalFunc(3)).toBe(9)
+    expect(testFunc).toHaveBeenCalledWith(3)
+    expect(condition).toHaveBeenCalled()
+  })
+
+  it('does not execute the function when the condition is false', () => {
+    const testFunc = jest.fn((x: number) => x * 3)
+    const condition = jest.fn(() => false)
+
+    const conditionalFunc = createConditionalExecutionFunction(testFunc, condition)
+
+    expect(conditionalFunc(3)).toBeUndefined()
+    expect(testFunc).not.toHaveBeenCalled()
+    expect(condition).toHaveBeenCalled()
+  })
+})
