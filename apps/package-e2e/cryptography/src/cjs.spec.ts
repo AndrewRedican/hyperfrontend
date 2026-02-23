a208d5d84cd699f0cecb1dc8b72f5d44ea4b746d
Andrew Redican <andrew.redican.mejia@gmail.com>
2026-02-23 05:39:37 +0000

    feat(e2e-lib-cryptography): add tests for outputs of lib-cryptography

---
+++ apps/package-e2e/cryptography/src/cjs.spec.ts
@@ -0,0 +1,93 @@
+/**
+ * CJS (CommonJS) E2E tests for @hyperfrontend/cryptography
+ * Tests that the package sub-entries are requireable and exports work correctly.
+ *
+ * Note: cryptography has platform-specific entries: ./browser, ./node, ./common
+ */
+
+describe('@hyperfrontend/cryptography CJS', () => {
+  describe('node sub-entry', () => {
+    it('should be requireable', () => {
+      // eslint-disable-next-line @typescript-eslint/no-require-imports
+      const nodeEntry = require('@hyperfrontend/cryptography/node')
+      expect(nodeEntry).toBeDefined()
+    })
+
+    it('should export createHash function', () => {
+      // eslint-disable-next-line @typescript-eslint/no-require-imports
+      const { createHash } = require('@hyperfrontend/cryptography/node')
+      expect(typeof createHash).toBe('function')
+    })
+
+    it('should export encrypt function', () => {
+      // eslint-disable-next-line @typescript-eslint/no-require-imports
+      const { encrypt } = require('@hyperfrontend/cryptography/node')
+      expect(typeof encrypt).toBe('function')
+    })
+
+    it('should export decrypt function', () => {
+      // eslint-disable-next-line @typescript-eslint/no-require-imports
+      const { decrypt } = require('@hyperfrontend/cryptography/node')
+      expect(typeof decrypt).toBe('function')
+    })
+
+    it('should export createVault function', () => {
+      // eslint-disable-next-line @typescript-eslint/no-require-imports
+      const { createVault } = require('@hyperfrontend/cryptography/node')
+      expect(typeof createVault).toBe('function')
+    })
+
+    it('should export generateKey function', () => {
+      // eslint-disable-next-line @typescript-eslint/no-require-imports
+      const { generateKey } = require('@hyperfrontend/cryptography/node')
+      expect(typeof generateKey).toBe('function')
+    })
+
+    it('should export getRandomValues function', () => {
+      // eslint-disable-next-line @typescript-eslint/no-require-imports
+      const { getRandomValues } = require('@hyperfrontend/cryptography/node')
+      expect(typeof getRandomValues).toBe('function')
+    })
+
+    it('should export getTimeBasedPassword function', () => {
+      // eslint-disable-next-line @typescript-eslint/no-require-imports
+      const { getTimeBasedPassword } = require('@hyperfrontend/cryptography/node')
+      expect(typeof getTimeBasedPassword).toBe('function')
+    })
+
+    it('should export isSHA256Hash function', () => {
+      // eslint-disable-next-line @typescript-eslint/no-require-imports
+      const { isSHA256Hash } = require('@hyperfrontend/cryptography/node')
+      expect(typeof isSHA256Hash).toBe('function')
+    })
+  })
+
+  describe('common sub-entry', () => {
+    it('should be requireable', () => {
+      // eslint-disable-next-line @typescript-eslint/no-require-imports
+      const commonEntry = require('@hyperfrontend/cryptography/common')
+      expect(commonEntry).toBeDefined()
+    })
+
+    it('should export isSHA256Hash function', () => {
+      // eslint-disable-next-line @typescript-eslint/no-require-imports
+      const { isSHA256Hash } = require('@hyperfrontend/cryptography/common')
+      expect(typeof isSHA256Hash).toBe('function')
+    })
+
+    it('isSHA256Hash should validate hash format', () => {
+      // eslint-disable-next-line @typescript-eslint/no-require-imports
+      const { isSHA256Hash } = require('@hyperfrontend/cryptography/common')
+
+      // Valid SHA256 hashes are 64 hex characters
+      const validHash = 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
+      const invalidHash = 'not-a-hash'
+
+      expect(isSHA256Hash(validHash)).toBe(true)
+      expect(isSHA256Hash(invalidHash)).toBe(false)
+    })
+  })
+
+  // Note: browser sub-entry is not tested in Node.js environment
+  // as it uses Web Crypto API which requires browser context
+})
