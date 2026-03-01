39b9f3f339a433558e1f74bf761316ca7ae56cb1
Andrew Redican <andrew.redican.mejia@gmail.com>
2026-03-01 10:16:45 +0000

    fix(lib-ui-utils): correct package exports and main entrypoint

---
+++ libs/utils/ui/src/index.ts
@@ -0,0 +1,17 @@
+/**
+ * \@hyperfrontend/ui-utils
+ *
+ * Modular DOM utilities for dynamic styling, gesture detection,
+ * element lifecycle, and color manipulation.
+ */
+
+export * from './audio'
+export * from './color'
+export * from './component'
+export * from './element'
+export * from './event'
+export * from './misc'
+export * from './mobile'
+export * from './selector'
+export * from './style'
+export * from './time'
