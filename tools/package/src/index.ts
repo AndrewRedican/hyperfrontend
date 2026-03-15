2b2527bf0349f3e4ff803580f1f19369415b1a42
Andrew Redican <andrew.redican.mejia@gmail.com>
2026-03-15 16:29:50 +0000

    chore(@hyperfrontend/workspace): add missing and organize ts path mappings in tsconfig.base.json

---
+++ tools/package/src/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Entry point for the \@hyperfrontend/package Nx plugin.
+ *
+ * This plugin provides build executors for hyperfrontend library packages.
+ *
+ * @see executors.json for available executors
+ * @see generators.json for available generators
+ */
+export {}
