2edc071d5d4a83f299837621eafb04eab8d6a36c
Andrew Redican <andrew.redican.mejia@gmail.com>
2026-03-31 02:02:54 +0000

    chore(lib-data-utils): remove to do from jsdoc that is misinterpreted by eslint rule

--- libs/utils/data/src/selective-copy.ts
+++ libs/utils/data/src/selective-copy.ts
@@ -140,7 +140,7 @@ export const selectiveCopyForCircularReferencesRecursive = <T extends Record<str
  * Creates a clone of the target. Options can be provided to selectively copy values.
  * Due to JavaScript language limitations context of bound functions is not known, thus functions cannot be reliably cloned.
  * This algorithm instead copies function references by default instead. For the same reason getters and setters are not replicate, only their
- * return values. This algorithm can replicate circular references, when configured to do so.
+ * return values. This algorithm can replicate circular references, when configured.
  * It supports other iterable data types, provided these have been made known using registerIterableClass.
  *
  * @param target - The value to clone
