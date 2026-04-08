5a29c6b064df0a15be626825d51212ca9a82fbee
Andrew Redican <andrew.redican.mejia@gmail.com>
2026-04-08 00:28:47 +0000

    docs(lib-list-utils): add missing api documentation

--- libs/utils/list/src/get-last-key-in-map.ts
+++ libs/utils/list/src/get-last-key-in-map.ts
@@ -1,4 +1,11 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+/**
+ * Retrieves the final key from a Map by converting keys to an array.
+ *
+ * @param map - The Map to extract the last key from
+ * @returns The key that was inserted last, or undefined if map is empty
+ */
 export const getLastKeyInMap = <K = any>(map: Map<K, any>): K => {
   const items = [...map.keys()]
   return <K>items[items.length - 1]
