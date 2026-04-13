9cb1a93cabd03fe76ad9854a62f2a7bf0a504153
Andrew Redican <andrew.redican.mejia@gmail.com>
2026-04-13 02:43:38 +0000

    refactor(lib-data-utils): remove star exports

--- libs/utils/data/src/index.ts
+++ libs/utils/data/src/index.ts
@@ -1,34 +1,55 @@
-export type * from './models'
-export type * from './get-value.model'
-export type * from './selective-copy.model'
-export * from './shared/consts'
-export * from './register-class-types'
-export * from './register-iterable-class'
-export * from './deregister-class-types'
-export * from './deregister-iterable-class'
-export * from './get-type'
-export * from './same-type'
-export * from './same-structure'
-export * from './is-iterable'
-export * from './is-iterable-type'
-export * from './is-marker'
-export * from './circular-reference'
-export * from './has-circular-reference'
-export * from './locate-circular-reference'
-export * from './reference-stack'
-export * from './is-identical'
-export * from './contains-keys'
-export * from './selective-copy'
-export * from './traverse'
-export * from './get-value'
-export * from './get-depth'
-export * from './locate-key'
-export * from './get-unique-keys'
-export * from './locate-text'
-export * from './rename-key'
-export * from './remove-key'
-export * from './replace-text'
-export * from './get-iterable-operators'
-export * from './get-iterable-types'
-export * from './get-keys-from-iterable'
-export * from './marker'
+export type {
+  Predicate,
+  DataType,
+  UnknownIterable,
+  UnknownIterableKey,
+  UnknownClass,
+  RegisteredIterableClassEntry,
+  IterableOperators,
+  Config,
+  ReferenceStack,
+  DepthConfig,
+  TraverseConfig,
+  Condition,
+  Callback,
+  TraversalArgs,
+  TraversalNonCircular,
+  TraversalCircular,
+  Traversal,
+  TraversalCreator,
+  Traverse,
+} from './models'
+export type { DefaultValueOptions } from './get-value.model'
+export type { SelectiveCopyPredicate, DataPointOperation, SelectiveCopyOptions, DataPoint, ReferenceLoop } from './selective-copy.model'
+export { registeredClasses, registeredIterableClasses, setConfig, getConfig } from './shared/consts'
+export { registerClassTypes } from './register-class-types'
+export { registerIterableClass } from './register-iterable-class'
+export { deregisterClassTypes } from './deregister-class-types'
+export { deregisterIterableClass } from './deregister-iterable-class'
+export { getType } from './get-type'
+export { sameType } from './same-type'
+export { sameStructure } from './same-structure'
+export { isIterable } from './is-iterable'
+export { isIterableType } from './is-iterable-type'
+export { isMarker } from './is-marker'
+export type { Location, Target, ICircularReference } from './circular-reference'
+export { CircularReference } from './circular-reference'
+export { hasCircularReference } from './has-circular-reference'
+export { locateCircularReferenceRecursive, locateCircularReference } from './locate-circular-reference'
+export { referenceStack } from './reference-stack'
+export { isIdentical } from './is-identical'
+export { containsKeys } from './contains-keys'
+export { selectiveCopyRecursive, selectiveCopyForCircularReferencesRecursive, selectiveCopy } from './selective-copy'
+export { createTraversal, traverse } from './traverse'
+export { getValue } from './get-value'
+export { getDepth } from './get-depth'
+export { locateKey } from './locate-key'
+export { getUniqueKeys } from './get-unique-keys'
+export { locateText } from './locate-text'
+export { renameKey } from './rename-key'
+export { removeKey } from './remove-key'
+export { replaceText } from './replace-text'
+export { getIterableOperators } from './get-iterable-operators'
+export { getIterableTypes } from './get-iterable-types'
+export { getKeysFromIterable } from './get-keys-from-iterable'
+export { marker } from './marker'
