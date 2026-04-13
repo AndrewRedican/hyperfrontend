dfe91fea49fc5d39f7163d35ac56f3d6d5fd0636
Andrew Redican <andrew.redican.mejia@gmail.com>
2026-04-13 03:23:50 +0000

    refactor(tool-package): remove star exports

--- tools/package/src/executors/build/lib/index.ts
+++ tools/package/src/executors/build/lib/index.ts
@@ -1,12 +1,74 @@
-export * from './types'
-export * from './assets'
-export * from './paths'
-export * from './entry-resolver'
-export * from './rollup-plugins'
-export * from './config-esm'
-export * from './config-cjs'
-export * from './config-iife'
-export * from './config-umd'
-export * from './declarations'
-export * from './package-json'
-export * from './logger'
+export type {
+  AssetConfig,
+  FormatEntryConfig,
+  ESMConfig,
+  CJSConfig,
+  IIFEConfig,
+  UMDConfig,
+  BuildExecutorOptions,
+  EntryPointCategory,
+  EntryPoint,
+  EntryPointDiscovery,
+  BuildContext,
+  OutputFormat,
+  ConditionalExport,
+  ExportValue,
+  RepositoryField,
+  BugsField,
+  AuthorField,
+  FundingField,
+  IIFEOutput,
+  UMDOutput,
+  PackageJson,
+  FormatOutputs,
+} from './types'
+export {
+  copyAssets,
+  copyDefaultAssets,
+  copyFundingAsset,
+  getDefaultAssetFiles,
+  getExternalDependencies,
+  collectThirdPartyLicenses,
+  generateThirdPartyLicensesContent,
+  copyThirdPartyLicensesAsset,
+} from './assets'
+export type { ThirdPartyLicenseEntry } from './assets'
+export {
+  resolveOutputPath,
+  resolveTsConfigPath,
+  joinConfigPath,
+  getRelativeProjectPath,
+  getSourcePath,
+  getEntryPointPath,
+  getStandardEntryPath,
+} from './paths'
+export {
+  discoverEntryPoints,
+  resolveEntries,
+  getEntryPointsByPlatform,
+  getSharedEntryPoints,
+} from './entry-resolver'
+export {
+  createNodeResolvePlugin,
+  createBrowserNodeResolvePlugin,
+  createCommonJsPlugin,
+  createTypescriptPlugin,
+  createBundleTypescriptPlugin,
+  createJsonPlugin,
+  createTerserPlugin,
+} from './rollup-plugins'
+export { createESMEntryConfig, createESMConfig } from './config-esm'
+export { createCJSEntryConfig, createCJSConfig } from './config-cjs'
+export { createIIFEEntryConfig, createIIFEConfig } from './config-iife'
+export { createUMDEntryConfig, createUMDConfig } from './config-umd'
+export { generateDeclarations, flattenDeclarationPaths } from './declarations'
+export {
+  readProjectPackageJson,
+  readRootPackageJson,
+  writeOutputPackageJson,
+  generateExportsFromFormats,
+  generatePackageJson,
+  hasFunding,
+} from './package-json'
+export type { LogLevel, Logger } from './logger'
+export { getLogger } from './logger'
