0bff899dd01f8e66f6ea566c9209bfc043c2931d
Andrew Redican <andrew.redican.mejia@gmail.com>
2026-04-14 21:55:23 +0000

    refactor(lib-state-machine): re-order exports

--- libs/state-machine/src/index.ts
+++ libs/state-machine/src/index.ts
@@ -3,11 +3,7 @@
  *
  * @module @hyperfrontend/state-machine
  */
-export { start, cancel, pause, success, fail, START, SUCCESS, FAIL, PAUSE, CANCEL } from './actions'
 export type { AsyncProcess } from './async-operation'
-export { AsyncOperation } from './async-operation'
-export { CoordinatedAsyncProcess } from './coordinated-async-operation'
-export { Events } from './events'
 export type {
   InitializingChangeCallback,
   ReadyChangeCallback,
@@ -15,8 +11,13 @@ export type {
   StoppingChangeCallback,
   ActiveChangeCallback,
 } from './lifecycle-aware-component'
-export { LifecycleAwareComponent } from './lifecycle-aware-component'
 export type { Action, Handlers, State, DerivedState, StateStatusDeriver, StateDeriver, Event } from './models'
+export type { Listener } from './store/store.model'
+export { start, cancel, pause, success, fail, START, SUCCESS, FAIL, PAUSE, CANCEL } from './actions'
+export { AsyncOperation } from './async-operation'
+export { CoordinatedAsyncProcess } from './coordinated-async-operation'
+export { Events } from './events'
+export { LifecycleAwareComponent } from './lifecycle-aware-component'
 export { event } from './models'
 export { rootReducer } from './reducer'
 export {
@@ -35,4 +36,3 @@ export {
 export { createInitialState } from './state'
 export { StateChange } from './state-change'
 export { Store } from './store/store'
-export type { Listener } from './store/store.model'
