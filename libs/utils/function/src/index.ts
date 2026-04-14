4cf65cb1eaf1527d2f97c77e08052f9f5046ef03
Andrew Redican <andrew.redican.mejia@gmail.com>
2026-04-14 19:00:40 +0000

    docs(lib-function-utils): add module header

--- libs/utils/function/src/index.ts
+++ libs/utils/function/src/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Higher-order function utilities for conditional execution, error handling, and one-time functions.
+ *
+ * @module @hyperfrontend/function-utils
+ */
 export { createConditionalExecutionFunction } from './create-conditional-execution-function'
 export { createErrorIgnoringFunction } from './create-error-ignoring-function'
 export { createRunOnceFunction } from './create-run-once-function'
