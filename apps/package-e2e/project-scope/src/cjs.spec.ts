801b8b2f91a0245f39674142f0a305ba32c9a4f3
Andrew Redican <andrew.redican.mejia@gmail.com>
2026-05-03 20:23:00 +0000

    chore(e2e-lib-project-scope): add nx heuristic api tests

--- apps/package-e2e/project-scope/src/cjs.spec.ts
+++ apps/package-e2e/project-scope/src/cjs.spec.ts
@@ -23,3 +23,47 @@ describe('@hyperfrontend/project-scope CJS', () => {
     expect(typeof analyzeProject).toBe('function')
   })
 })
+
+describe('NX heuristics (CJS)', () => {
+  it('should export isNxWorkspace function', () => {
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    const { isNxWorkspace } = require('@hyperfrontend/project-scope')
+    expect(typeof isNxWorkspace).toBe('function')
+  })
+
+  it('should export findNxWorkspaceRoot function', () => {
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    const { findNxWorkspaceRoot } = require('@hyperfrontend/project-scope')
+    expect(typeof findNxWorkspaceRoot).toBe('function')
+  })
+
+  it('should export isNxProject function', () => {
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    const { isNxProject } = require('@hyperfrontend/project-scope')
+    expect(typeof isNxProject).toBe('function')
+  })
+
+  it('should export getNxWorkspaceInfo function', () => {
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    const { getNxWorkspaceInfo } = require('@hyperfrontend/project-scope')
+    expect(typeof getNxWorkspaceInfo).toBe('function')
+  })
+
+  it('should export discoverNxProjects function', () => {
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    const { discoverNxProjects } = require('@hyperfrontend/project-scope')
+    expect(typeof discoverNxProjects).toBe('function')
+  })
+
+  it('should export buildSimpleProjectGraph function', () => {
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    const { buildSimpleProjectGraph } = require('@hyperfrontend/project-scope')
+    expect(typeof buildSimpleProjectGraph).toBe('function')
+  })
+
+  it('should export nxDetector function', () => {
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    const { nxDetector } = require('@hyperfrontend/project-scope')
+    expect(typeof nxDetector).toBe('function')
+  })
+})
