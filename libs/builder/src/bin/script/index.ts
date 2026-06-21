cccb405f1a0d6161d4856db463d449d65370713b
Andrew Redican <andrew.redican.mejia@gmail.com>
2026-06-21 11:08:01 +0000

    refactor(lib-builder): delete dead output-derived wireBinFieldInPackageJson; synthesizeBinField is sole bin source

--- libs/builder/src/bin/script/index.ts
+++ libs/builder/src/bin/script/index.ts
@@ -1,10 +1,9 @@
 /**
  * JS bin synthesis primitives: rollup-driven bundling with shebang + bootstrap footer
- * + chmod, plus the package.json#bin wiring helper.
+ * + chmod.
  *
  * @module @hyperfrontend/builder/bin/script
  */
 export type { DefaultBootstrapOptions } from './bootstrap-footer'
 export { defaultBootstrap } from './bootstrap-footer'
 export { buildJsBin } from './build-bin'
-export { wireBinFieldInPackageJson } from './wire-package-bin'
