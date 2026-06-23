be2ac3125abff90feb812105e2c136e92f5a343f
Andrew Redican <andrew.redican.mejia@gmail.com>
2026-06-23 05:29:49 +0000

    chore(@hyperfrontend/workspace): disable lib-builder-implicit-dependency

--- eslint.base.config.cjs
+++ eslint.base.config.cjs
@@ -259,7 +259,7 @@ module.exports = [
       'workspace/lib-project-bundle-config': 'error',
       'workspace/lib-project-version-targets': 'error',
       'workspace/lib-e2e-project-required': 'error',
-      'workspace/lib-builder-implicit-dependency': 'error',
+      'workspace/lib-builder-implicit-dependency': 'off', // disabled for now, as it cause cyclic project dependency in NX graph
     },
   },
   {
