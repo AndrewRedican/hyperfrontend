3bd73f73eb44e62b9d6a54f30d3dac9156aa4577
Andrew Redican <andrew.redican.mejia@gmail.com>
2026-07-03 20:40:47 +0000

    feat(lib-features): export contract option types from the hostee entry

--- libs/features/src/hostee/index.ts
+++ libs/features/src/hostee/index.ts
@@ -3,5 +3,6 @@
  *
  * @module @hyperfrontend/features/hostee
  */
+export type { ActionDescription, FeatureContract, FeatureOptions } from '../shared/types'
 export type { FeatureHandle } from './types'
 export { createFeature } from './create-feature'
