90a5e6dee294723b326df1f25ca272b790b995f6
Andrew Redican <andrew.redican.mejia@gmail.com>
2026-08-18 01:51:39 +0000

    docs(e2e-lib-immutable-api-utils): backtick the package name in spec headers

--- apps/package-e2e/immutable-api-utils/src/cjs.spec.ts
+++ apps/package-e2e/immutable-api-utils/src/cjs.spec.ts
@@ -1,5 +1,5 @@
 /**
- * CJS (CommonJS) E2E tests for @hyperfrontend/immutable-api-utils
+ * CJS (CommonJS) E2E tests for `@hyperfrontend/immutable-api-utils`
  * Tests that the package is requireable and exports work correctly.
  */
 
