28405c10f05c1e2b7b550b62df8c357e3e549c4f
Andrew Redican <andrew.redican.mejia@gmail.com>
2026-08-18 01:52:01 +0000

    docs(e2e-lib-random-generator-utils): backtick the package name in spec headers

--- apps/package-e2e/random-generator-utils/src/esm.spec.ts
+++ apps/package-e2e/random-generator-utils/src/esm.spec.ts
@@ -1,5 +1,5 @@
 /**
- * ESM (ES Modules) E2E tests for @hyperfrontend/random-generator-utils
+ * ESM (ES Modules) E2E tests for `@hyperfrontend/random-generator-utils`
  * Tests that the package is importable and exports work correctly.
  */
 
