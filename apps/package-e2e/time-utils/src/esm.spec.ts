ddef5a8a6806429e240216fad4bffd78003300fa
Andrew Redican <andrew.redican.mejia@gmail.com>
2026-08-18 01:52:10 +0000

    docs(e2e-lib-time-utils): backtick the package name in spec headers

--- apps/package-e2e/time-utils/src/esm.spec.ts
+++ apps/package-e2e/time-utils/src/esm.spec.ts
@@ -1,5 +1,5 @@
 /**
- * ESM (ES Modules) E2E tests for @hyperfrontend/time-utils
+ * ESM (ES Modules) E2E tests for `@hyperfrontend/time-utils`
  * Tests that the package is importable and exports work correctly.
  */
 
