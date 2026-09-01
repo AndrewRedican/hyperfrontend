c5500c9857c5a458e262de0988d89dbde99a342b
Andrew Redican <andrew.redican.mejia@gmail.com>
2026-09-01 22:34:27 +0000

    test(e2e-lib-data-utils): run the e2e formats on the node test runner

--- apps/package-e2e/data-utils/src/esm.spec.ts
+++ apps/package-e2e/data-utils/src/esm.spec.ts
@@ -3,6 +3,8 @@
  * Tests that the package is importable and exports work correctly.
  */
 
+import { describe, it, expect } from '@hyperfrontend/testing'
+
 describe('@hyperfrontend/data-utils ESM', () => {
   it('is importable', async () => {
     const dataUtils = await import('@hyperfrontend/data-utils')
