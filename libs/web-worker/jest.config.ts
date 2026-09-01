fe7910680f59fe41f2fe330fc5d59c3f23c5a23f
Andrew Redican <andrew.redican.mejia@gmail.com>
2026-09-01 02:30:49 +0000

    test(lib-web-worker): run the suite on the node test runner

--- libs/web-worker/jest.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from 'jest'
-
-export default {
-  displayName: 'web-worker',
-  preset: '../../jest.preset.cjs',
-  moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/libs/web-worker',
-  testEnvironment: 'jsdom',
-} as Config
