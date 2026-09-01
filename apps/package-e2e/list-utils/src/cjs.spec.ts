c68dc811b632a6200915492f4d5a4e5deb675729
Andrew Redican <andrew.redican.mejia@gmail.com>
2026-09-01 22:35:50 +0000

    test(e2e-lib-list-utils): run the cjs spec in strict mode as ts-jest did

--- apps/package-e2e/list-utils/src/cjs.spec.ts
+++ apps/package-e2e/list-utils/src/cjs.spec.ts
@@ -3,6 +3,8 @@
  * Tests that the package is requireable and exports work correctly.
  */
 
+'use strict'
+
 const { describe, expect, it } = require('@hyperfrontend/testing')
 
 describe('@hyperfrontend/list-utils CJS', () => {
